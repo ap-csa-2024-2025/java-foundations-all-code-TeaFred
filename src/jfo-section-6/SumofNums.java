544d51e3b19d12959a6eab456d57f65e3d113ff1
TeaFred <fhtruong100@student.hbuhsd.edu>
2025-05-27 18:17:18 +0000

    fdgdfgdfg

--- src/jfo-section-6/SumofNums.java
+++ src/jfo-section-6/SumofNums.java
@@ -4,17 +4,17 @@ public class SumofNums {
 
     public static void main(String args[]) {
         Scanner console = new Scanner(System.in);
-        int sum = 0;
-        System.out.print("Enter a number (-1 to quit): ");
-        int number = console.nextInt();
+        int sum = 0; 
+        int number = 0;  
 
-        while (number != -1) {
-            sum = sum + number;     // moved to top of loop
+        do
+        {
             System.out.print("Enter a number (-1 to quit): ");
-            number = console.nextInt();
-        }
+            number = console.nextInt();   
+            sum = sum + number;     
+        } while (number != -1);
 
-        System.out.println("The sum is " + sum);
+        System.out.println("The sum is " + (sum + 1));
 
     }
 }
