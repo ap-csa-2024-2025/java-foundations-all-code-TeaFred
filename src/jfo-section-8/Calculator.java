5eb0caf2b0900cb496abaa1ecc774e4155c41a70
TeaFred <fhtruong100@student.hbuhsd.edu>
2025-05-29 18:20:17 +0000

    fdgdf

--- src/jfo-section-8/Calculator.java
+++ src/jfo-section-8/Calculator.java
@@ -1,10 +1,26 @@
-public class Calculator {
-    public int add(int x, int y){
+public class Calculator 
+{
+    public int add(int x, int y)
+    {
         return x + y;
     }
     
     // This method could throw an ArithmeticException
-    public double divide(int x, int y) {
+    public double divide(int x, int y) 
+    {
         return x / y;
     }
+
+    try
+    {
+        double divResult = Calculator.divide(15,0);
+        System.out.println("Division result: " + divResult);
+    }
+
+    catch (ArithmeticException Exception)
+    {
+        System.out.println("No division by 0.");
+        Exception.printStackTrace();
+    }
+    System.out.println("Yay?");
 }
